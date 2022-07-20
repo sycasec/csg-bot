8e531cab3194e17d2366d657e4c6b0bbf1d4112c
Kale <69748956+Kaelefor@users.noreply.github.com>
2022-07-20 22:54:34 +0800

    added request into chatbotcontrol

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+import request from 'request';
 
 let postWebhook = (req, res) => {
     // Parse the request body from the POST
@@ -17,6 +18,14 @@ let postWebhook = (req, res) => {
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the even is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message){
+              handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+              handlePostback(sender_psid, webhook_event.postback);
+            }
             
         });
 
@@ -57,6 +66,19 @@ let getWebhook = (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response); 
 
 }
 
@@ -67,6 +89,27 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v6.0/me/messages",
+    "qs": { "access_token": process.env.PAGE_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!, my message: ${response}');
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
   
 }
 
